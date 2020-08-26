dac3b8c9312630c9b60cf097b5a12fb22ecb23f2
Sai Charan <charan0017@gmail.com>
2020-08-26 07:25:15 +0530

    BugFix for time lag; occurred when tab is inactive

--- index.js
+++ index.js
@@ -9,24 +9,41 @@ const circle = document.querySelector('#circle');
 const perimeter = circle.getAttribute('r') * Math.PI * 2;
 circle.setAttribute('stroke-dasharray', perimeter);
 
-durationInput.value = getRemainingTime();
+let timer;
 
-let duration;
-const timer = new Timer(durationInput, {
-  onStart(totalDuration) {
-    duration = totalDuration;
-  },
-  onTick(timeRemaining, totalDuration, percentRemaining) {
-    circle.setAttribute('stroke-dashoffset', (perimeter * timeRemaining) / totalDuration - perimeter);
-    const percentageText = `${percentRemaining.toFixed(2)} %`;
-    remainingPercentText.innerHTML = percentageText;
-    const remainingInterval = getIntervalFromSeconds(durationInput.value);
-    remainingHrs.innerHTML = remainingInterval.hrs;
-    remainingMins.innerHTML = remainingInterval.mins;
-    remainingSecs.innerHTML = remainingInterval.secs;
-    document.title = `${percentageText} - ${remainingInterval.hrs}.${remainingInterval.mins}.${remainingInterval.secs}`;
-  },
-  onComplete() {
-    console.log('complete');
-  },
+function createNewTimer() {
+  durationInput.value = getRemainingTime();
+  timer = new Timer(durationInput, {
+    onStart(totalDuration) {
+      console.log(`A new coutdown has started, totalSeconds: ${totalDuration}`);
+    },
+    onTick(timeRemaining, totalDuration, percentRemaining) {
+      circle.setAttribute('stroke-dashoffset', (perimeter * timeRemaining) / totalDuration - perimeter);
+      const percentageText = `${percentRemaining.toFixed(2)} %`;
+      remainingPercentText.innerHTML = percentageText;
+      const remainingInterval = getIntervalFromSeconds(durationInput.value);
+      remainingHrs.innerHTML = remainingInterval.hrs;
+      remainingMins.innerHTML = remainingInterval.mins;
+      remainingSecs.innerHTML = remainingInterval.secs;
+      document.title = `${percentageText} - ${remainingInterval.hrs}.${remainingInterval.mins}.${remainingInterval.secs}`;
+    },
+    onComplete() {
+      console.log('complete');
+    },
+  });
+}
+
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    // console.log('tab is now inactive');
+    if (!timer) return;
+    timer.pause();
+    timer = null;
+  } else {
+    // console.log('tab is active again');
+    if (timer) return;
+    createNewTimer();
+  }
 });
+
+if (!timer) createNewTimer();
